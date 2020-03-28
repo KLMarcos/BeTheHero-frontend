ce14540700cee4ffddc56423b2de23e0a73f0bfd
Marcos Vinicius Oliveira Tomaz <marcoshmpm1@gmail.com>
2020-03-28 03:50:56 -0300

    Criada interfaces para cadastro de ongs e incidentes. Conectado frontend ao backend

---
+++ src/pages/Logon/index.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import { FiLogIn } from 'react-icons/fi'
+
+import api from './../../services/api'
+
+import HeroesImage from '../../assets/heroes.png'
+import LogoImage from '../../assets/logo.svg'
+import './style.css'
+
+export default function Logon() {
+  const [id, setId] = useState('')
+
+  const history = useHistory()
+
+  async function handleLogin(e) {
+    e.preventDefault()
+
+    try {
+      const {
+        data: { name: ongName },
+      } = await api.post('sessions', { id })
+
+      localStorage.setItem('ongId', id)
+      localStorage.setItem('ongName', ongName)
+
+      history.push('/profile')
+    } catch (error) {
+      alert('falha no login, tente novamente')
+    }
+  }
+
+  return (
+    <div className="logon-container">
+      <section className="form">
+        <img src={LogoImage} alt="Be The Hero" />
+
+        <form onSubmit={handleLogin}>
+          <h1>Faça seu logon</h1>
+          <input
+            type="text"
+            placeholder="Sua ID"
+            value={id}
+            onChange={(e) => setId(e.target.value)}
+          />
+          <button className="button" type="submit">
+            Entrar
+          </button>
+
+          <Link className="back-link" to="/register">
+            <FiLogIn size={16} color="#e02041" />
+            Não tenho cadastro
+          </Link>
+        </form>
+      </section>
+
+      <img src={HeroesImage} alt="Heroes" />
+    </div>
+  )
+}
